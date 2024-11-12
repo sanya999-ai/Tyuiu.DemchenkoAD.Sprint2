ca86a07ba0a0848c8613379cb0d8256a937aa66e
Александра Демченко <Tyuiu.DemchenkoAD@outlook.com>
2024-11-12 15:52:21 +0500

    Task4

--- Tyuiu.DemchenkoAD.Sprint2.Task4.V12.Lib/DataService.cs
+++ Tyuiu.DemchenkoAD.Sprint2.Task4.V12.Lib/DataService.cs
@@ -5,7 +5,14 @@ namespace Tyuiu.DemchenkoAD.Sprint2.Task4.V12.Lib
     {
         public double Calculate(double x, double y)
         {
-            double z = Math.Sqrt(x) > y * 2 ? Math.Pow(7 + (2 / Math.Pow(y, 2)), x) : (3 * Math.Pow(x, 2) - Math.Pow(Math.Cos(y), 2) + 10) / (Math.Pow(y, 2) - Math.Pow(Math.Sin(x), 2) + 12);
+            double a, b;
+
+            a = Math.Pow((7 + (2 / Math.Pow(y, 2))), x);
+            b = ((3 * Math.Pow(x, 2) - Math.Pow(Math.Cos(y), 2) + 10) / (Math.Pow(y, 2) - Math.Pow(Math.Sin(x), 2) + 12));
+
+
+            double z = (Math.Sqrt(x) > y * 2) ? a : b;
+
             return Math.Round(z, 3);
         }
     }
